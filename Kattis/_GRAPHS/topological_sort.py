80430596221155709d531d765b9dc52ff9234330
OleVikLysne <oleviklysne@gmail.com>
2024-07-09 18:15:21 +0200

    updated topological_sort

--- Kattis/_GRAPHS/topological_sort.py
+++ Kattis/_GRAPHS/topological_sort.py
@@ -1,21 +1,31 @@
-def topological_sort(graph):
+from sys import setrecursionlimit
+setrecursionlimit(2**30)
+
+def topological_sort(g: list[list[int]], roots: list[int]):
+    if not roots:
+        return False
+
     topo_order = []
-    visited = [False]*len(graph)
-    recursion_stack = set()
+    visited = [False]*len(g)
+    rec_stack = set()
 
-    def dfs(node):
-        if node in recursion_stack:
-            raise ValueError("Graph is not a DAG")
-        if not visited[node]:
-            visited[node] = True
-            recursion_stack.add(node)
+    def dfs(v):
+        if v in rec_stack:
+            return False # not a DAG
+        if not visited[v]:
+            visited[v] = True
+            rec_stack.add(v)
 
-            for neighbor in graph[node]:
-                dfs(neighbor)
-            recursion_stack.remove(node)
+            for u in g[v]:
+                if not dfs(u):
+                    return False
+            rec_stack.remove(v)
 
-            topo_order.append(node)
+            topo_order.append(v)
+        return True
 
-    dfs(0)
+    for root in roots:
+        if not dfs(root):
+            return False
     topo_order.reverse()
     return topo_order
