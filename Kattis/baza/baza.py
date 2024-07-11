d36adcf46d388b9c8fb949d20655be956a9cf9a5
OleVikLysne <oleviklysne@gmail.com>
2024-07-11 23:13:05 +0200

    minor baza python speedup

--- Kattis/baza/baza.py
+++ Kattis/baza/baza.py
@@ -1,50 +1,43 @@
-import sys
+from sys import stdin, stdout
+
 
 class Node:
     def __init__(self):
         self.count = 0
         self.children = {}
 
+    def add(self, word):
+        self.count += 1
+        s = self.count
+        current = self
+        for char in word:
+            current = current.children.setdefault(char, Node())
+            current.count += 1
+            s += current.count
+        current.children["*"] = s
+        return s
 
-ROOT = Node()
-
-def add(word):
-    ROOT.count += 1
-    s = ROOT.count
-    current = ROOT
-    for char in word:
-        current = current.children.setdefault(char, Node())
-        current.count += 1
-        s += current.count
-    return s
-
-
-def find(word):
-    s = ROOT.count
-    current = ROOT
-    for char in word:
-        current = current.children.get(char)
-        if current is None:
-            return s
-        s += current.count
-    return s
+    def find(self, word):
+        s = self.count
+        current = self
+        for char in word:
+            current = current.children.get(char)
+            if current is None:
+                return s
+            s += current.count
+        return current.children.get("*", s)
 
 
-n = int(sys.stdin.readline())
-words = {}
+n = int(stdin.readline())
+trie = Node()
 for _ in range(n):
-    i = sys.stdin.readline()
-    word = tuple(ord(i[x]) for x in range(len(i)-1))
-    s = add(word)
-    words[word] = s
+    inp = stdin.readline()
+    word = tuple(ord(inp[x]) for x in range(len(inp) - 1))
+    s = trie.add(word)
 
 
-q = int(sys.stdin.readline())
+q = int(stdin.readline())
 for _ in range(q):
-    i = sys.stdin.readline()
-    query_word = tuple(ord(i[x]) for x in range(len(i)-1))
-    s = words.get(query_word)
-    if s is None:
-        sys.stdout.write(str(find(query_word))+"\n")
-    else:
-        sys.stdout.write(str(s)+"\n")
+    inp = stdin.readline()
+    query_word = tuple(ord(inp[x]) for x in range(len(inp) - 1))
+    stdout.write(str(trie.find(query_word)) + " ")
