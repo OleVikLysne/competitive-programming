286f136a798841ad871e765ac79ce2f0b3f64962
OleVikLysne <oleviklysne@gmail.com>
2024-07-11 14:02:03 +0200

    finish conservation

--- Kattis/solutions/conservation/conservation.py
+++ Kattis/solutions/conservation/conservation.py
@@ -1,62 +1,38 @@
-from sys import stdin, stdout
+from sys import stdin
 
-def topological_sort(graph, root=0):
-    topo_order = []
-    visited = [False]*len(graph)
 
-    def dfs(node):
-        if not visited[node]:
-            visited[node] = True
-            for neighbor in graph[node]:
-                dfs(neighbor)
-            topo_order.append(node)
-    dfs(root)
-    topo_order.reverse()
-    return topo_order
+def search(start):
+    stacks = ([], [])
 
-def rearrange(top_sort):
-    i = 0
-    n = len(top_sort)
-    while i + 1 < n:
-        j = i + 1
-        if mask[top_sort[i]] == mask[top_sort[j]]:
-            i += 1
-            continue
-        k = i + 2
-        while k < n and k not in g[j]:
-            if mask[top_sort[k]] == mask[top_sort[i]]:
-                top_sort[k], top_sort[j] = top_sort[j], top_sort[k]
-                break
-            k += 1
+    deg = [x for x in degree]
 
-        i += 1
-            
+    for i in range(n):
+        if deg[i] == 0:
+            stacks[mask[i]].append(i)
 
-def count(top_sort):
-    k = mask[top_sort[0]]
-    c = 0
-    for val in top_sort[1:]:
-        if mask[val] != k:
-            c += 1
-            k = mask[val]
+    i = start
+    c = -1
+    while stacks[0] or stacks[1]:
+        while stacks[i]:
+            v = stacks[i].pop()
+            for u in g[v]:
+                deg[u] -= 1
+                if deg[u] == 0:
+                    stacks[mask[u]].append(u)
+        c += 1
+        i = (i + 1) % 2
+
     return c
 
 
-            
-t = int(stdin.readline())
-for _ in range(t):
+for _ in range(int(stdin.readline())):
     n, m = map(int, stdin.readline().split())
-    mask = [int(x) for x in stdin.readline().split()]
-    g = [set() for _ in range(n)]
-    roots = set(range(n))
+    mask = [int(x) - 1 for x in stdin.readline().split()]
+    g = [[] for _ in range(n)]
+    degree = [0] * n
     for _ in range(m):
-        i, j = [int(x)-1 for x in stdin.readline().split()]
-        g[i].add(j)
-        if j in roots:
-            roots.remove(j)
-    s = 0
-    for root in roots:
-        top_sort = topological_sort(g, root)
-        rearrange(top_sort)
-        s += count(top_sort)
-    print(s)
+        i, j = [int(x) - 1 for x in stdin.readline().split()]
+        g[i].append(j)
+        degree[j] += 1
+
+    print(min(search(0), search(1)))
