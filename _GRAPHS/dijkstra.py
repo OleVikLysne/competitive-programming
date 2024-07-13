086e29feaa200a6ef5359f5dc79ca71614278f9d
OleVikLysne <oleviklysne@gmail.com>
2024-07-13 22:03:09 +0200

    updated dijkstra to include path calc

--- _GRAPHS/dijkstra.py
+++ _GRAPHS/dijkstra.py
@@ -1,16 +1,30 @@
 import heapq
 
 n: int = ...
-g: list[tuple[int, float | int]] = ...
+g: list[list[tuple[int, float | int]]] = ...
 
 def dijkstra(source):
-    dist = [float("inf") for _ in range(n)]
+    dist = [float("inf")]*n
     dist[source] = 0
+    prev = [-1]*n
     pq = [(0, source)]
     while pq:
         _, u = heapq.heappop(pq)
         for (v, w) in g[u]:
             if dist[v] > dist[u]+w:
                 dist[v] = dist[u]+w
+                prev[v] = u
                 heapq.heappush(pq, (dist[v], v))
-    return dist
+    return dist, prev
+
+
+def get_path(dist, prev, start, target):
+    v = target
+    if dist[v] == float("inf"):
+        return False
+    path = [v]
+    while v != start:
+        v = prev[v]
+        path.append(v)
+    path.reverse()
+    return path
