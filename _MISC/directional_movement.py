6946f64e127ed571558eba3a4a1f49a09546b267
OleVikLysne <oleviklysne@gmail.com>
2024-08-11 00:01:48 +0200

    added chess knight moves

--- _MISC/directional_movement.py
+++ _MISC/directional_movement.py
@@ -11,3 +11,9 @@ def valid_moves(i, j):
     for x, y in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
         if 0 <= x < rows and 0 <= y < cols:
             yield x, y
+
+
+def valid_chess_knight_moves(i, j):
+    for x, y in ((i+2, j+1), (i-2, j+1), (i+2, j-1), (i-2, j-1), (i+1, j+2), (i+1, j-2), (i-1, j+2), (i-1, j-2)):
+        if 0 <= x < rows and 0 <= y < cols:
+            yield x, y
