d9f0d91280ed1ae685f6740b7a5f4786f334e390
OleVikLysne <oleviklysne@gmail.com>
2024-09-11 15:41:07 +0200

    rewrote feast for cats to use prims algorithm for testing/practise

--- Kattis/a_feast_for_cats/cats.py
+++ Kattis/a_feast_for_cats/cats.py
@@ -1,46 +1,38 @@
 import sys; input=sys.stdin.readline
-
-def find(parent, i):
-    if parent[i] == i:
-        return i
-    return find(parent, parent[i])
+import heapq
 
 
-def union(parent, rank, x, y):
-    x = find(parent, x)
-    y = find(parent, y)
-    if rank[x] > rank[y]:
-        parent[y] = x
-        rank[x] += 1
-    else:
-        parent[x] = y
-        rank[y] += 1
-
-
-def kruskal(edges, num_nodes):
-    edges.sort(key=lambda x: x[2], reverse=True)
-    parent = [x for x in range(num_nodes)]
-    rank = [0] * num_nodes
+def prim(g: list[tuple[int, int]]):
+    num_nodes = len(g)
+    edges = [(x[1], x[0]) for x in g[0]]
+    heapq.heapify(edges)
+    visited = [False]*num_nodes
+    visited[0] = True
     tree_sum = 0
     n = 0
     while n < num_nodes - 1:
-        u, v, w = edges.pop()
-        x = find(parent, u)
-        y = find(parent, v)
-        if x != y:
-            tree_sum += w
-            n += 1
-            union(parent, rank, x, y)
-
+        w, v = heapq.heappop(edges)
+        if visited[v]:
+            continue
+        visited[v] = True
+        tree_sum += w
+        n += 1
+        for u, w in g[v]:
+            if visited[u]:
+                continue
+            heapq.heappush(edges, (w, u))
     return tree_sum
 
 
 for _ in range(int(input())):
     m, c = map(int, input().split())
-    edges = []
+    g = [[] for _ in range(c)]
     for _ in range((c*(c-1)//2)):
-        edges.append(tuple(map(int, input().split())))
-    if kruskal(edges, c) + c <= m:
+        u, v, w = map(int, input().split())
+        g[u].append((v, w))
+        g[v].append((u, w))
+
+    if prim(g) + c <= m:
         print("yes")
     else:
         print("no")
