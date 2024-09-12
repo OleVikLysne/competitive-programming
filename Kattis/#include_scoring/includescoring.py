b40ad8052a0776d73648fd1c065426715c61aef3
OleVikLysne <oleviklysne@gmail.com>
2024-09-12 11:08:18 +0200

    simplified include scoring

--- Kattis/#include_scoring/includescoring.py
+++ Kattis/#include_scoring/includescoring.py
@@ -1,58 +1,29 @@
 import sys; input=sys.stdin.readline
 from math import ceil
-from collections import defaultdict
 
-m = {
-    1:100,
-    2:75,
-    3:60,
-    4:50,
-    5:45,
-    6:40,
-    7:36,
-    8:32,
-    9:29,
-    10:26,
-    11:24,
-    12:22,
-    13:20,
-    14:18,
-    15:16
-}
-score_map = defaultdict(int)
-for k, v in m.items():
-    score_map[k] = v
-
-for i in range(16, 31):
-    score_map[i] = 31 - i
+SCORE_MAP = [100, 75, 60, 50, 45, 40, 36, 32, 29, 26, 24, 22, 20, 18, 16]
+SCORE_MAP.extend(range(15, 0, -1))
 
 n = int(input())
 l = []
 scores = [0]*n
 for i in range(n):
     s, p, f, o = map(int, input().split())
-    scores[i] = o
-    l.append((-s, p, f, i))
+    scores[i] += o
+    l.append(((-s, p, f), i))
 l.sort()
 
-
+j = 0
 for i in range(n):
-    c = 1
-    s = score_map[i+1]
-    for j in range(i-1, -1, -1):
-        if l[i][:3] != l[j][:3]:
+    if j == i:
+        if i >= 30:
             break
-        s += score_map[j+1]
-        c += 1
-    for j in range(i+1, n):
-        if l[i][:3] != l[j][:3]:
-            break
-        s += score_map[j+1]
-        c += 1
-    if i >= 30 and c == 1:
-        break
-
-    scores[l[i][-1]] += ceil(s/c)
-
+        while j < n and l[i][0] == l[j][0]:
+            j += 1
+        cur_score = 0
+        for k in range(i, min(30, j)):
+            cur_score += SCORE_MAP[k]
+        cur_score = ceil(cur_score/(j-i))
+    scores[l[i][1]] += cur_score
 
 print(*scores)
