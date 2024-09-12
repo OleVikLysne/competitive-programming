a99e6d3faf9f75bd8de611b5d91ad396e145dfb9
OleVikLysne <oleviklysne@gmail.com>
2024-09-12 23:53:41 +0200

    fixed mistake in bellman ford implementation

--- _GRAPHS/bellman_ford.py
+++ _GRAPHS/bellman_ford.py
@@ -1,15 +1,18 @@
+g: list[list[tuple[int, float | int]]] = ...
+
 def bellman_ford(s, g, n):
-    dist, pred = [float("inf")]*n, {}
+    dist = [float("inf")]*n
+    pred = [-1]*n
     dist[s] = 0
     for _ in range(n-1):
         for parent in range(n):
-            for (child, w) in g[parent]:
+            for child, w in g[parent]:
                 if dist[child] > dist[parent] + w:
                     dist[child] = dist[parent] + w
                     pred[child] = parent
 
     for parent in range(n):
-        for (child, w) in n[parent]:
+        for child, w in g[parent]:
             if dist[child] > dist[parent] + w:
                 return True # negative cycle
     return dist, pred
