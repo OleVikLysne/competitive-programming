0b865bfa8f87d6c43e6519e8d465bed1be108869
OleVikLysne <oleviklysne@gmail.com>
2024-09-15 13:29:30 +0200

    added source/target version of dijkstra

--- _GRAPHS/dijkstra.py
+++ _GRAPHS/dijkstra.py
@@ -1,27 +1,47 @@
 import heapq
 
-n: int = ...
 g: list[list[tuple[int, float | int]]] = ...
 
+
+# dijkstra to all nodes
 def dijkstra(source):
+    n = len(g)
     dist = [float("inf")]*n
     dist[source] = 0
     prev = [-1]*n
     pq = [(0, source)]
     while pq:
-        _, u = heapq.heappop(pq)
-        for v, w in g[u]:
-            if dist[v] > dist[u]+w:
-                dist[v] = dist[u]+w
-                prev[v] = u
-                heapq.heappush(pq, (dist[v], v))
+        _, v = heapq.heappop(pq)
+        for u, w in g[v]:
+            if dist[u] > dist[v]+w:
+                dist[u] = dist[v]+w
+                prev[u] = v
+                heapq.heappush(pq, (dist[u], u))
     return dist, prev
 
 
-def get_path(dist, prev, start, target):
+# dijkstra to specific target node
+def dijkstra(source, target):
+    n = len(g)
+    dist = [float("inf")]*n
+    dist[source] = 0
+    prev = [-1]*n
+    pq = [(0, source)]
+    while pq:
+        d, v = heapq.heappop(pq)
+        if v == target:
+            return d, prev
+        for u, w in g[v]:
+            if dist[u] > dist[v]+w:
+                dist[u] = dist[v]+w
+                prev[u] = v
+                heapq.heappush(pq, (dist[u], u))
+    return False
+
+
+# fails if the target wasn't reached
+def get_path(prev, start, target):
     v = target
-    if dist[v] == float("inf"):
-        return False
     path = [v]
     while v != start:
         v = prev[v]
