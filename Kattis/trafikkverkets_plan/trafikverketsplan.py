0097e285bad1ffcf9afb54aa6be5d0ab98548afd
OleVikLysne <oleviklysne@gmail.com>
2024-09-25 10:10:53 +0200

    tourists and trafikverketsplan tested rewritten with new general-purpose lowest common ancestor implementation

--- Kattis/trafikkverkets_plan/trafikverketsplan.py
+++ Kattis/trafikkverkets_plan/trafikverketsplan.py
@@ -1,16 +1,17 @@
 import sys; input=sys.stdin.readline
 
-def anc(x, k):
+def anc(v, k):
     if k == 0:
-        return x
+        return v
     log_k = (k-1).bit_length()
     if k & (k-1) == 0:
-        return anc_matrix[log_k][x]
+        return anc_matrix[log_k][v]
 
-    for j in range(log_k-1, -1, -1):
-        if k >> j & 1:
-            x = anc_matrix[j][x]
-    return x
+    for i in range(log_k-1, -1, -1):
+        if k >> i & 1:
+            v = anc_matrix[i][v]
+    return v
+
 
 def lca(u, v):
     if depth[v] > depth[u]:
@@ -24,9 +25,34 @@ def lca(u, v):
         if anc_matrix[i][u] != anc_matrix[i][v]:
             u = anc_matrix[i][u]
             v = anc_matrix[i][v]
+
+    return anc_matrix[0][v]
+
+
+def build_anc_matrix(g: list[list[int]], n: int): # g has length n+1 because 1-indexed
+    log_n = (n-1).bit_length()
+    depth = [-1]*(n+1)
+    depth[1] = depth[0] = 0
+    anc_matrix = [[-1 for _ in range(n+1)] for _ in range(log_n)]
+    anc_matrix[0][1] = anc_matrix[0][0] = 0
+
+    stack = [1]
+    while stack:
+        v = stack.pop()
+        for u, t in g[v]:
+            if depth[u] == -1:
+                depth[u] = depth[v] + 1
+                anc_matrix[0][u] = v
+                stack.append(u)
+                dir_jumps[u][t] = dir_jumps[v][t] + 1
+
+    for i in range(1, log_n):
+        for j in range(1, n+1):
+            anc_matrix[i][j] = anc_matrix[i-1][anc_matrix[i-1][j]]
+
+    return depth, anc_matrix
     
-    ancestor = anc_matrix[0][v]
-    return max(1, ancestor)
+
 
 def is_path(start, target):
     ancestor = lca(start, target)
@@ -44,23 +70,9 @@ for _ in range(n-1):
     g[a].append((b, 1))
     g[b].append((a, 0))
 
-depth = [-1]*(n+1)
-anc_matrix = [[0 for _ in range(n+1)] for _ in range(log_n + 1)]
-dir_jumps = [[0, 0] for _ in range(n+1)]
-depth[1] = 0
-stack = [1]
-while stack:
-    v = stack.pop()
-    for u, t in g[v]:
-        if depth[u] == -1:
-            depth[u] = depth[v] + 1
-            anc_matrix[0][u] = v
-            dir_jumps[u][t] = dir_jumps[v][t] + 1
-            stack.append(u)
 
-for i in range(1, log_n):
-    for j in range(1, n+1):
-        anc_matrix[i][j] = anc_matrix[i-1][anc_matrix[i-1][j]]
+dir_jumps = [[0, 0] for _ in range(n+1)]
+depth, anc_matrix = build_anc_matrix(g, n)
 
 q = int(input())
 for _ in range(q):
