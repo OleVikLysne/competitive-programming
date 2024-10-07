62e322942762e33c9054c5f6fc8917ba51650d58
OleVikLysne <oleviklysne@gmail.com>
2024-10-07 18:22:29 +0200

    made segmenttree uglier to optimize for speed (was necessary to solve double deck)

--- _ARRAYS/segment_tree.py
+++ _ARRAYS/segment_tree.py
@@ -1,12 +1,14 @@
 class SegmentTree:
-    def __init__(self, array, op=sum, pad = False):
-        self.op = op
-        if op == max:
-            val = -2**62
-        elif op == min:
-            val = 2**62
-        else:
+    def __init__(self, array, op=sum, pad=False):
+        if op == sum:
             val = 0
+            self.op = lambda x, y: x + y
+        elif op == max:
+            val = -2**62
+            self.op = op
+        else: # op == min
+            val = 2**62
+            self.op = op
 
         if pad:
             self.n = 2**((len(array)-1).bit_length())
@@ -17,64 +19,40 @@ class SegmentTree:
             self.T = [val]*self.n + array
 
         for i in range(self.n-1, 0, -1):
-            self.T[i] = self.op((
-                self.T[self.left(i)],
-                self.T[self.right(i)]
-            ))
+            self.T[i] = self.op(
+                self.T[2*i],
+                self.T[2*i+1]
+            )
 
     def __repr__(self):
         return str(self.T)
 
-    @property
-    def root(self):
-        return self.T[1]
-    
-    def index(self, i):
-        return self.n + i
-
-    def reverse_index(self, i):
-        return i - self.n
-    
-    def is_leaf(self, i):
-        return i >= self.n
-    
-    def parent(self, i):
-        return i >> 1
-
-    def sibling(self, i):
-        return i+1 if i % 2 == 0 else i-1
-    
-    def left(self, i):
-        return 2 * i
-
-    def right(self, i):
-        return 2 * i + 1
-
     def update(self, i, val):
-        i = self.index(i)
+        i += self.n
         self.T[i] = val
-        while (i := self.parent(i)) > 0:
-            self.T[i] = self.op((
-                self.T[self.left(i)],
-                self.T[self.right(i)]
-            ))
+        while i > 1:
+            i >>= 1
+            self.T[i] = self.op(
+                self.T[2*i],
+                self.T[2*i+1]
+            )
 
     # [l, r], inclusive on both sides
-    def _query(self, l, r):
-        if l == r:
-            yield self.T[self.index(l)]
-            return
-
-        l = self.index(l)
-        r = self.index(r)
-        yield self.T[l]
-        yield self.T[r]
-        while (pl := self.parent(l)) != (pr := self.parent(r)):
-            if l % 2 == 0:
-                yield self.T[l+1]
-            if r % 2 == 1:
-                yield self.T[r-1]
-            l, r = pl, pr
-
     def query(self, l, r):
-        return self.op(self._query(l, r))
+        l += self.n
+        r += self.n
+        if l == r:
+            return self.T[l]
+
+        res = self.op(self.T[l], self.T[r])
+        pl = l >> 1
+        pr = r >> 1
+        while pl != pr:
+            if l & 1 == 0:
+                res = self.op(res, self.T[l+1])
+            if r & 1 == 1:
+                res = self.op(res, self.T[r-1])
+            l, r = pl, pr
+            pl >>= 1
+            pr >>= 1
+        return res
