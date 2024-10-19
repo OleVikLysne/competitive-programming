463db842521396eb792c8f52de0cd25e006d38d4
OleVikLysne <oleviklysne@gmail.com>
2024-10-19 08:57:23 +0200

    minor refactor

--- _ARRAYS/segment_tree.py
+++ _ARRAYS/segment_tree.py
@@ -13,31 +13,31 @@ class SegmentTree:
         if pad:
             self.n = 2**((len(array)-1).bit_length())
             pad_amount = self.n - len(array)
-            self.T: list = [val]*self.n
-            self.T.extend(array)
-            self.T.extend(val for _ in range(pad_amount))
+            self.tree: list = [val]*self.n
+            self.tree.extend(array)
+            self.tree.extend(val for _ in range(pad_amount))
         else:
             self.n = len(array)
-            self.T: list = [val]*self.n
-            self.T.extend(array)
+            self.tree: list = [val]*self.n
+            self.tree.extend(array)
 
         for i in range(self.n-1, 0, -1):
-            self.T[i] = self.op(
-                self.T[2*i],
-                self.T[2*i+1]
+            self.tree[i] = self.op(
+                self.tree[2*i],
+                self.tree[2*i+1]
             )
 
     def __repr__(self):
-        return str(self.T)
+        return str(self.tree)
 
     def update(self, i, val):
         i += self.n
-        self.T[i] = val
+        self.tree[i] = val
         while i > 1:
             i >>= 1
-            self.T[i] = self.op(
-                self.T[2*i],
-                self.T[2*i+1]
+            self.tree[i] = self.op(
+                self.tree[2*i],
+                self.tree[2*i+1]
             )
 
     # [l, r], inclusive on both sides
@@ -45,16 +45,16 @@ class SegmentTree:
         l += self.n
         r += self.n
         if l == r:
-            return self.T[l]
+            return self.tree[l]
 
-        res = self.op(self.T[l], self.T[r])
+        res = self.op(self.tree[l], self.tree[r])
         pl = l >> 1
         pr = r >> 1
         while pl != pr:
             if l & 1 == 0:
-                res = self.op(res, self.T[l+1])
+                res = self.op(res, self.tree[l+1])
             if r & 1 == 1:
-                res = self.op(res, self.T[r-1])
+                res = self.op(res, self.tree[r-1])
             l, r = pl, pr
             pl >>= 1
             pr >>= 1
