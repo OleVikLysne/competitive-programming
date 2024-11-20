111107effcda0819465dab24451b56fee824e7d1
OleVikLysne <oleviklysne@gmail.com>
2024-11-20 11:15:56 +0100

    changed function order

--- _GRAPHS/strongly_connected_components.py
+++ _GRAPHS/strongly_connected_components.py
@@ -1,5 +1,45 @@
 
 
+############
+# KOSARAJU #
+############
+
+def kosaraju(g: list[list[int]], rev_g: list[list[int]]):
+    n = len(g)
+    visited = [False]*n
+    order = []
+    sccs = []
+    component = [-1]*n
+    def dfs(v):
+        visited[v] = True
+        for u in g[v]:
+            if not visited[u]:
+                dfs(u)
+        order.append(v)
+
+    def rev_dfs(v, root, comp = None):
+        if comp is None:
+            comp = []
+        component[v] = root
+        comp.append(v)
+        for u in rev_g[v]:
+            if component[u] == -1:
+                comp = rev_dfs(u, root, comp)
+        return comp
+
+    for v in range(n):
+        if not visited[v]:
+            dfs(v)
+
+    for i in range(len(order)-1, -1, -1):
+        v = order[i]
+        if component[v] == -1:
+            sccs.append(rev_dfs(v, v))
+    
+    return sccs
+
+
+
 ##########
 # TARJAN #
 ##########
@@ -41,47 +81,6 @@ def tarjan(g: list[list[int]]):
         if pre_order[v] == -1:
             dfs(v)
     return sccs
-
-
-############
-# KOSARAJU #
-############
-
-def kosaraju(g: list[list[int]], rev_g: list[list[int]]):
-    n = len(g)
-    visited = [False]*n
-    order = []
-    sccs = []
-    component = [-1]*n
-    def dfs(v):
-        visited[v] = True
-        for u in g[v]:
-            if not visited[u]:
-                dfs(u)
-        order.append(v)
-
-    def rev_dfs(v, root, comp = None):
-        if comp is None:
-            comp = []
-        component[v] = root
-        comp.append(v)
-        for u in rev_g[v]:
-            if component[u] == -1:
-                comp = rev_dfs(u, root, comp)
-        return comp
-
-    for v in range(n):
-        if not visited[v]:
-            dfs(v)
-
-    for i in range(len(order)-1, -1, -1):
-        v = order[i]
-        if component[v] == -1:
-            sccs.append(rev_dfs(v, v))
-    
-    return sccs
-
-
 
 
 
