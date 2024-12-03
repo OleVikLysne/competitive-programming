7bc9d193fbc235e486590ec16f8d8b3bca03ead3
OleVikLysne <oleviklysne@gmail.com>
2024-12-03 14:10:57 +0100

    fix

--- aoc24/3/part2.py
+++ aoc24/3/part2.py
@@ -1,17 +1,17 @@
 import re
 import sys
 s = 0
-b = True
+bool = True
 for line in sys.stdin: 
     for x in re.finditer(r"(mul\([0-9]{1,3},[0-9]{1,3}\))|do\(\)|don't\(\)", line):
         x = x.group()
         if x == "don't()":
-            b = False
+            bool = False
             continue
         elif x == "do()":
-            b = True
+            bool = True
             continue
-        if not b:
+        if not bool:
             continue
         x = x.strip("mul()")
         a, b = (int(y) for y in x.split(","))
