57e2c0a2f94fe075493cbace5297e3a6200945d6
OleVikLysne <oleviklysne@gmail.com>
2024-12-10 11:47:54 +0100

    solved d10

---
+++ aoc24/d10/part1.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+
+const DELTAS: [(isize, isize); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+fn search(i: isize, j: isize, grid: &[Vec<u8>], visited: &mut HashSet<(isize, isize)>) -> usize {
+    if grid[i as usize][j as usize] == 9 {
+        if ! visited.insert((i, j)) {
+            return 0
+        }
+        return 1
+    }
+    let mut res = 0;
+    for (di, dj) in DELTAS {
+        let (x, y) = (i+di, j+dj);
+        if 0 <= x && x < grid.len() as isize
+        && 0 <= y && y < grid[0].len() as isize
+        && grid[x as usize][y as usize] == grid[i as usize][j as usize] + 1 {
+            res += search(x, y, grid, visited);
+        }
+    }
+    return res;
+}
+
+fn main() {
+    let mut grid: Vec<Vec<u8>> = Vec::new();
+    for line in std::io::stdin().lines().map(|x| x.unwrap()) {
+        grid.push(line.chars().map(|x| x as u8 - 48).collect());
+    }
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut res = 0;
+    for i in 0..rows {
+        for j in 0..cols {
+            if grid[i][j] == 0 {
+                res += search(i as isize, j as isize, &grid, &mut HashSet::new());
+            }
+        }
+    }
+    println!("{}", res);
+}
