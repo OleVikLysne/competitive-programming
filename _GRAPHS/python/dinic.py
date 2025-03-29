54b630fefbb423a08f06d361c958f380c5fcffd0
OleVikLysne <oleviklysne@gmail.com>
2025-03-29 18:18:04 +0100

    updated dinic and ford fulkerson, removed edmonds karp

--- _GRAPHS/python/dinic.py
+++ _GRAPHS/python/dinic.py
@@ -1,57 +1,52 @@
-from collections import deque
-node_count: int = ...
-weight_matrix: list[list[int]] = ... # contains the initial edge capacities
-flow_matrix: list[list[int]] = ... # initialized to all zeros, contains the pushed flow for recovering the edges
+capacity: list[list[int]] = ... # contains the initial edge capacities
+source: int = ...
+sink: int = ...
 
-def bfs(source, sink, level):
+
+from collections import deque
+
+INF = float("inf")
+
+def bfs(source, sink, level, capacity):
+    n = len(capacity)
     q = deque([source])
     level[source] = 0
     while q:
         v = q.popleft()
-        for u in range(node_count):
-              cap = weight_matrix[v][u]
+        for u in range(n):
+              cap = capacity[v][u]
               if cap > 0 and level[u] < 0:
                   level[u] = level[v] + 1
                   q.append(u)
     return level[sink] != -1
 
-def dfs(v, flow, sink, ptr, level):
+def dfs(v, flow, sink, ptr, level, capacity):
     if v == sink or flow == 0:
         return flow
-    
-    for u in range(ptr[v], node_count):
-        cap = weight_matrix[v][u]
+    n = len(capacity)
+    for u in range(ptr[v], n):
+        cap = capacity[v][u]
         if level[u] == level[v]+1 and cap > 0:
-            pushed_flow = dfs(u, min(flow, cap), sink, ptr, level)
+            pushed_flow = dfs(u, min(flow, cap), sink, ptr, level, capacity)
             if pushed_flow > 0:
-                weight_matrix[v][u] -= pushed_flow
-                weight_matrix[u][v] += pushed_flow
-                flow_matrix[v][u] += pushed_flow
-                flow_matrix[u][v] -= pushed_flow
+                capacity[v][u] -= pushed_flow
+                capacity[u][v] += pushed_flow
                 return pushed_flow
         ptr[v] += 1
     return 0
 
-def dinic(source, sink):
+def dinic(source, sink, capacity):
+    n = len(capacity)
     total_flow = 0
-    level = [-1]*node_count
+    level = [-1]*n
     level[source] = 0
-    while bfs(source, sink, level):
-        ptr = [0]*node_count
+    while bfs(source, sink, level, capacity):
+        ptr = [0]*n
         while True:
-            flow = dfs(source, float("inf"), sink, ptr, level)
+            flow = dfs(source, INF, sink, ptr, level, capacity)
             if flow == 0:
                 break
             total_flow += flow
-        level = [-1]*node_count
+        level = [-1]*n
         level[source] = 0
     return total_flow
-
-
-def get_flow_edges(flow_matrix):
-    l = []
-    for i in range(node_count):
-        for j in range(node_count):
-            if flow_matrix[i][j] > 0:
-                l.append((i, j, flow_matrix[i][j]))
-    return l
--- _GRAPHS/python/ford_fulkerson.py
+++ _GRAPHS/python/ford_fulkerson.py
@@ -1,33 +1,44 @@
-n: int = ...
-weight_matrix: list[list[int]] = ...
-node_count: int = n+2 # depends on the task
-source = n # depends on the task
-sink = n+1 # depends on the task
+capacity: list[list[int]] = ... # contains the initial edge capacities
+g: list[list[int]] = ... # undirected and unweighted version of the graph
+source: int = ...
+sink: int = ...
 
 
-def dfs(v, flow, visited):
-    visited[v] = True
-    if v == sink:
-        return flow
-    for u, cap in enumerate(weight_matrix[v]):
-        if cap == 0 or visited[u]:
-            continue
-        res = dfs(u, min(flow, cap), visited)
-        if res != 0:
-            weight_matrix[v][u] -= res
-            weight_matrix[u][v] += res
-            return res
+INF = float("inf")
 
-    return 0
+def push_flow(source, sink, g, capacity):
+    n = len(g)
+    stack = [(source, INF)]
+    #queue = deque([(source, INF)])
+    pred = [-1]*n
+    pred[source] = source
+    while stack:
+        v, flow = stack.pop()
+        if v == sink:
+            break
+        for u in g[v]:
+            if pred[u] != -1:
+                continue
+            if capacity[v][u] > 0:
+                stack.append((u, min(flow, capacity[v][u])))
+                pred[u] = v
+    else:
+        return 0
 
-def ford_fulkerson(source):
+    u = sink
+    while u != source:
+        v = pred[u]
+        capacity[v][u] -= flow
+        capacity[u][v] += flow
+        u = v
+    return flow
+
+
+def ford_fulkerson(source, sink, g, capacity):
     total_flow = 0
     while True:
-        visited = [False for _ in range(node_count)]
-        flow = dfs(source, float("inf"), visited)
+        flow = push_flow(source, sink, g, capacity)
         total_flow += flow
-        if flow == float("inf") or flow == 0:
+        if flow == INF or flow == 0:
             break
-
     return total_flow
-
