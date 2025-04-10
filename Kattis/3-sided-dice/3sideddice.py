bb390bb2231561550ef9b54caf58a566321c11d8
OleVikLysne <oleviklysne@gmail.com>
2025-04-10 12:59:45 +0200

    added vec2 annotations and stricly inside polygon check

--- Kattis/3-sided-dice/3sideddice.py
+++ Kattis/3-sided-dice/3sideddice.py
@@ -1,4 +1,4 @@
-import sys
+from __future__ import annotations
 import math
 from typing import Union
 
@@ -9,7 +9,11 @@ class Vec2:
 
     @property
     def length(self) -> float:
-        return math.sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.squared_length)
+    
+    @property
+    def squared_length(self) -> Union[float, int]:
+        return self.x**2 + self.y**2
 
     @property
     def is_zero_vec(self):
@@ -22,13 +26,13 @@ class Vec2:
             return self / self.length
         self /= self.length
 
-    def dot(self, other) -> Union[float, int]:
+    def dot(self, other: Vec2) -> Union[float, int]:
         return self.x * other.x + self.y * other.y
 
-    def cross(self, other) -> Union[float, int]:
+    def cross(self, other: Vec2) -> Union[float, int]:
         return self.x * other.y - self.y * other.x
 
-    def angle(self, other, radians=False) -> float:
+    def angle(self, other: Vec2, radians=False) -> float:
         res = math.acos(
             self.normalize(in_place=False).dot(other.normalize(in_place=False))
         )
@@ -42,26 +46,26 @@ class Vec2:
             return res
         return math.degrees(res)
 
-    def orient(self, v1, v2) -> Union[float, int]:
+    def orient(self, v1: Vec2, v2: Vec2) -> Union[float, int]:
         return (v1.x-self.x) * (v2.y - self.y) - (v1.y - self.y) * (v2.x-self.x)
         # v3 = v1 - self
         # v4 = v2 - self
         # return v3.cross(v4)
 
-    def right_of(self, other, origin=None) -> bool:
+    def right_of(self, other: Vec2, origin=None) -> bool:
         if origin is None:
             return self.cross(other) > 0
         return origin.orient(self, other) > 0
 
-    def left_of(self, other, origin=None) -> bool:
+    def left_of(self, other: Vec2, origin=None) -> bool:
         if origin is None:
             return self.cross(other) < 0
         return origin.orient(self, other) < 0
 
-    def inc_right_of(self, other, origin=None) -> bool:
+    def inc_right_of(self, other: Vec2, origin=None) -> bool:
         return not self.left_of(other, origin)
 
-    def inc_left_of(self, other, origin=None) -> bool:
+    def inc_left_of(self, other: Vec2, origin=None) -> bool:
         return not self.right_of(other, origin)
 
     def rotate(self, theta: Union[float, int], radians=False):
@@ -86,68 +90,54 @@ class Vec2:
             self.x * sin_theta + self.y * cos_theta,
         )
 
-    def dist(self, other) -> float:
+    def dist(self, other: Vec2) -> float:
         return math.sqrt(self.squared_dist(other))
 
-    def squared_dist(self, other) -> Union[float, int]:
+    def squared_dist(self, other: Vec2) -> Union[float, int]:
         return (self.x - other.x)**2 + (self.y - other.y)**2
 
-    def __mul__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
-        elif isinstance(other, (int, float)):
-            return self.__class__(self.x * other, self.y * other)
+    def __mul__(self, other: Union[float, int]):
+        return self.__class__(self.x * other, self.y * other)
 
-    def __truediv__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
-        elif isinstance(other, (int, float)):
-            return self.__class__(self.x / other, self.y / other)
+    def __truediv__(self, other: Union[float, int]):
+        return self.__class__(self.x / other, self.y / other)
 
-    def __add__(self, other):
-        if not isinstance(other, Vec2):
-            raise Exception()
+    def __add__(self, other: Vec2):
         return self.__class__(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
-        if not isinstance(other, Vec2):
-            raise Exception()
+    def __sub__(self, other: Vec2):
         return self.__class__(self.x - other.x, self.y - other.y)
 
     def __neg__(self):
         return self.__class__(-self.x, -self.y)
 
-    def __imul__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
+    def __imul__(self, other: Union[float, int]):
         self.x *= other
         self.y *= other
         return self
 
-    def __itruediv__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
+    def __itruediv__(self, other: Union[float, int]):
         self.x /= other
         self.y /= other
         return self
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Vec2):
         self.x += other.x
         self.y += other.y
         return self
 
-    def __isub__(self, other):
+    def __isub__(self, other: Vec2):
         self.x -= other.x
         self.y -= other.x
         return self
 
-    def __radd__(self, other):
+    def __radd__(self, other: Vec2):
         return self.__add__(other)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other: Vec2):
         return self.__sub__(other)
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: Union[float, int]):
         return self.__mul__(other)
 
     def __eq__(self, other):
@@ -155,18 +145,15 @@ class Vec2:
             return self.x == other.x and self.y == other.y
         return False
 
-    def __lt__(self, other):
+    def __lt__(self, other: Vec2):
         if self.x == other.x:
             return self.y < other.y
         return self.x < other.x
 
-    def __gt__(self, other):
+    def __gt__(self, other: Vec2):
         if self.x == other.x:
             return self.y > other.y
         return self.x > other.x
-
-    def __le__(self, other):
-        return self < other or self == other
 
     def __repr__(self):
         return f"Vec2({self.x} {self.y})"
@@ -180,32 +167,43 @@ class Vec2:
 def collinear(a: Vec2, b: Vec2, c: Vec2):
     return a.orient(b, c) == 0
 
+def is_on(p: Vec2, p1: Vec2, p2: Vec2):
+    if not collinear(p1, p2, p):
+        return False
+    if p1.x != p2.x:
+        return min(p1.x, p2.x) <= p.x <= max(p1.x, p2.x)
+    return min(p1.y, p2.y) <= p.y <= max(p1.y, p2.y)
+
 def inside_polygon(poly: list[Vec2], p: Vec2) -> bool:
+    n = len(poly)
     inside = False
     x, y = p.x, p.y
-    for i in range(len(poly)):
-        j = (i + 1) % len(poly)
-        # if is_on(p, poly[i], poly[j]):
-        #     return True
-        i_x, i_y = poly[i].x, poly[i].y
-        j_x, j_y = poly[j].x, poly[j].y
-        if (i_y > y) != (j_y > y) and x < (j_x - i_x) * (y - i_y) / (j_y - i_y) + i_x:
+    for i in range(n):
+        j = (i + 1) % n
+        if is_on(p, poly[i], poly[j]):
+            return True
+        ix, iy = poly[i].x, poly[i].y
+        jx, jy = poly[j].x, poly[j].y
+        if (iy > y) != (jy > y) and x < (jx - ix) * (y - iy) / (jy - iy) + ix:
             inside = not inside
     return inside
 
-
-def solve(poly: list[Vec2], p: Vec2):
-    if collinear(*poly):
-        return collinear(poly[0], poly[1], p) and min(a.length for a in poly) < p.length < max(a.length for a in poly)
+def strictly_inside_polygon(poly: list[Vec2], p: Vec2) -> bool:
     n = len(poly)
     for i in range(n):
-        p2, p3 = poly[i], poly[(i+1)%n]
-        if collinear(p, p2, p3):
+        j = (i + 1) % n
+        if collinear(p, poly[i], poly[j]):
             return False
     return inside_polygon(poly, p)
 
 
-input = sys.stdin.readline
+def solve(poly: list[Vec2], p: Vec2):
+    if collinear(*poly):
+        return collinear(poly[0], poly[1], p) and min(a.squared_length for a in poly) < p.squared_length < max(a.squared_length for a in poly)
+    return strictly_inside_polygon(poly, p)
+
+
+import sys; input = sys.stdin.readline
 while True:
     x1, y1, z1 = map(int, input().split())
     if x1 == y1 == z1 == 0: break
--- _MATH_AND_GEMOETRY/vec2.py
+++ _MATH_AND_GEMOETRY/vec2.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import math
 from typing import Union
 
@@ -8,7 +9,11 @@ class Vec2:
 
     @property
     def length(self) -> float:
-        return math.sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.squared_length)
+    
+    @property
+    def squared_length(self) -> Union[float, int]:
+        return self.x**2 + self.y**2
 
     @property
     def is_zero_vec(self):
@@ -21,13 +26,13 @@ class Vec2:
             return self / self.length
         self /= self.length
 
-    def dot(self, other) -> Union[float, int]:
+    def dot(self, other: Vec2) -> Union[float, int]:
         return self.x * other.x + self.y * other.y
 
-    def cross(self, other) -> Union[float, int]:
+    def cross(self, other: Vec2) -> Union[float, int]:
         return self.x * other.y - self.y * other.x
 
-    def angle(self, other, radians=False) -> float:
+    def angle(self, other: Vec2, radians=False) -> float:
         res = math.acos(
             self.normalize(in_place=False).dot(other.normalize(in_place=False))
         )
@@ -41,26 +46,26 @@ class Vec2:
             return res
         return math.degrees(res)
 
-    def orient(self, v1, v2) -> Union[float, int]:
+    def orient(self, v1: Vec2, v2: Vec2) -> Union[float, int]:
         return (v1.x-self.x) * (v2.y - self.y) - (v1.y - self.y) * (v2.x-self.x)
         # v3 = v1 - self
         # v4 = v2 - self
         # return v3.cross(v4)
 
-    def right_of(self, other, origin=None) -> bool:
+    def right_of(self, other: Vec2, origin=None) -> bool:
         if origin is None:
             return self.cross(other) > 0
         return origin.orient(self, other) > 0
 
-    def left_of(self, other, origin=None) -> bool:
+    def left_of(self, other: Vec2, origin=None) -> bool:
         if origin is None:
             return self.cross(other) < 0
         return origin.orient(self, other) < 0
 
-    def inc_right_of(self, other, origin=None) -> bool:
+    def inc_right_of(self, other: Vec2, origin=None) -> bool:
         return not self.left_of(other, origin)
 
-    def inc_left_of(self, other, origin=None) -> bool:
+    def inc_left_of(self, other: Vec2, origin=None) -> bool:
         return not self.right_of(other, origin)
 
     def rotate(self, theta: Union[float, int], radians=False):
@@ -85,68 +90,54 @@ class Vec2:
             self.x * sin_theta + self.y * cos_theta,
         )
 
-    def dist(self, other) -> float:
+    def dist(self, other: Vec2) -> float:
         return math.sqrt(self.squared_dist(other))
 
-    def squared_dist(self, other) -> Union[float, int]:
+    def squared_dist(self, other: Vec2) -> Union[float, int]:
         return (self.x - other.x)**2 + (self.y - other.y)**2
 
-    def __mul__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
-        elif isinstance(other, (int, float)):
-            return self.__class__(self.x * other, self.y * other)
+    def __mul__(self, other: Union[float, int]):
+        return self.__class__(self.x * other, self.y * other)
 
-    def __truediv__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
-        elif isinstance(other, (int, float)):
-            return self.__class__(self.x / other, self.y / other)
+    def __truediv__(self, other: Union[float, int]):
+        return self.__class__(self.x / other, self.y / other)
 
-    def __add__(self, other):
-        if not isinstance(other, Vec2):
-            raise Exception()
+    def __add__(self, other: Vec2):
         return self.__class__(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other):
-        if not isinstance(other, Vec2):
-            raise Exception()
+    def __sub__(self, other: Vec2):
         return self.__class__(self.x - other.x, self.y - other.y)
 
     def __neg__(self):
         return self.__class__(-self.x, -self.y)
 
-    def __imul__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
+    def __imul__(self, other: Union[float, int]):
         self.x *= other
         self.y *= other
         return self
 
-    def __itruediv__(self, other):
-        if isinstance(other, Vec2):
-            raise Exception("Not implemented")
+    def __itruediv__(self, other: Union[float, int]):
         self.x /= other
         self.y /= other
         return self
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Vec2):
         self.x += other.x
         self.y += other.y
         return self
 
-    def __isub__(self, other):
+    def __isub__(self, other: Vec2):
         self.x -= other.x
         self.y -= other.x
         return self
 
-    def __radd__(self, other):
+    def __radd__(self, other: Vec2):
         return self.__add__(other)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other: Vec2):
         return self.__sub__(other)
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: Union[float, int]):
         return self.__mul__(other)
 
     def __eq__(self, other):
@@ -154,12 +145,12 @@ class Vec2:
             return self.x == other.x and self.y == other.y
         return False
 
-    def __lt__(self, other):
+    def __lt__(self, other: Vec2):
         if self.x == other.x:
             return self.y < other.y
         return self.x < other.x
 
-    def __gt__(self, other):
+    def __gt__(self, other: Vec2):
         if self.x == other.x:
             return self.y > other.y
         return self.x > other.x
@@ -226,11 +217,10 @@ def shoelace(poly: list[Vec2]):
 def poly_circumference(poly: list[Vec2]):
     n = len(poly)
     s = 0
-    for i in range(len(poly)):
+    for i in range(n):
         j = (i + 1) % n
         s += poly[i].dist(poly[j])
     return s
-
 
 def collinear(a: Vec2, b: Vec2, c: Vec2):
     return a.orient(b, c) == 0
@@ -239,22 +229,30 @@ def is_on(p: Vec2, p1: Vec2, p2: Vec2):
     if not collinear(p1, p2, p):
         return False
     if p1.x != p2.x:
-        return p1.x <= p.x <= p2.x or p2.x <= p.x <= p1.x
-    return p1.y <= p.y <= p2.y or p2.y <= p.y <= p1.y
+        return min(p1.x, p2.x) <= p.x <= max(p1.x, p2.x)
+    return min(p1.y, p2.y) <= p.y <= max(p1.y, p2.y)
 
 def inside_polygon(poly: list[Vec2], p: Vec2) -> bool:
+    n = len(poly)
     inside = False
     x, y = p.x, p.y
-    for i in range(len(poly)):
-        j = (i + 1) % len(poly)
+    for i in range(n):
+        j = (i + 1) % n
         if is_on(p, poly[i], poly[j]):
             return True
-        i_x, i_y = poly[i].x, poly[i].y
-        j_x, j_y = poly[j].x, poly[j].y
-        if (i_y > y) != (j_y > y) and x < (j_x - i_x) * (y - i_y) / (j_y - i_y) + i_x:
+        ix, iy = poly[i].x, poly[i].y
+        jx, jy = poly[j].x, poly[j].y
+        if (iy > y) != (jy > y) and x < (jx - ix) * (y - iy) / (jy - iy) + ix:
             inside = not inside
     return inside
 
+def strictly_inside_polygon(poly: list[Vec2], p: Vec2) -> bool:
+    n = len(poly)
+    for i in range(n):
+        j = (i + 1) % n
+        if collinear(p, poly[i], poly[j]):
+            return False
+    return inside_polygon(poly, p)
 
 def polygon_intersect(poly1: list[Vec2], poly2: list[Vec2]):
     def get_inside(poly1, poly2):
@@ -280,7 +278,6 @@ def polygon_intersect(poly1: list[Vec2], poly2: list[Vec2]):
     # and finally, compute the convex hull to get the polygon itself
     return graham_scan(points)
 
-
 def rotating_calipers(hull: list[Vec2]):
     n = len(hull)
     max_distance = 0
@@ -292,7 +289,6 @@ def rotating_calipers(hull: list[Vec2]):
             k = (k + 1) % n
         max_distance = max(max_distance, hull[i].squared_dist(hull[j]))
     return math.sqrt(max_distance)
-
 
 def circle_intersection_area(x1, y1, r1, x2, y2, r2):
     d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -309,7 +305,6 @@ def circle_intersection_area(x1, y1, r1, x2, y2, r2):
         * math.sqrt((-d + r1 + r2) * (d + r1 - r2) * (d - r1 + r2) * (d + r1 + r2))
     )
     return a
-
 
 def inside_convex(hull: list[Vec2], p: Vec2) -> bool:
     """
