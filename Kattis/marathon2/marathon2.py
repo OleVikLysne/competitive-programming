6ec5fdcee94e69c12f1397f97040eab8f40f44e8
OleVikLysne <oleviklysne@gmail.com>
2025-04-22 11:40:50 +0200

    solved marathon2 in rust

--- Kattis/marathon2/marathon2.py
+++ Kattis/marathon2/marathon2.py
@@ -1,5 +1,7 @@
 import sys; input=sys.stdin.readline
 
+INF = 2**60
+
 n, x, y = map(int, input().split())
 r, j = map(int, input().split())
 scale = r*j # scale to avoid floating point
@@ -12,27 +14,21 @@ stations.add(0)
 stations = sorted(stations)
 n = len(stations)
 
-jump = [-1]*n
-k = 0
-for i in range(n-1):
-    while k < n-1 and stations[k] - stations[i] <= x:
-        k += 1
-    jump[i] = k
-
-dp = [2**60]*n
+dp = [INF]*n
 dp[0] = 0
+idx = 0
 for i in range(n-1):
-    for k in (jump[i]-1, jump[i], n-1):
-        if k == i: continue
+    if dp[i] == INF: continue
+    while idx < n-2 and stations[idx] - stations[i] <= x:
+        idx += 1
+    for k in (idx-1, idx, n-1):
         d = stations[k] - stations[i]
         run_d = min(d, x)
         jog_d = d - run_d
-        next_t = dp[i] + jog_d // j + run_d // r
-        if k != n-1:
-            next_t += y
+        next_t = dp[i] + jog_d // j + run_d // r + y
         dp[k] = min(dp[k], next_t)
 
-res = dp[-1]
+res = dp[-1] - y
 res //= scale # convert back to un-scaled time
 h, s = divmod(res, 3600)
 m, s = divmod(s, 60)
