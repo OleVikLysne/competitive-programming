49ed97869180dbca7738712a9dd619ffae387a84
OleVikLysne <oleviklysne@gmail.com>
2025-05-04 17:02:20 +0200

    update to roll hash

--- _STRINGS/rolling_hash.py
+++ _STRINGS/rolling_hash.py
@@ -1,5 +1,13 @@
-B = 2**31 - 1 # 2**61-1 # 10**9+7 # 2987583780930787
+B = 2**31 - 1 # 2**61-1 # 10**9+7 # 2987583780930787 # 100123456789
 A = 31
+
+def rolling_hash(string):
+    hashed_str = 0
+    k = len(string)
+    for i in range(k):
+        hashed_str += string[i] * pow(A, k - i - 1, B)
+        hashed_str %= B
+    return hashed_str
 
 def roll_hash(string, k):
     n = len(string)
@@ -18,16 +26,3 @@ def roll_hash(string, k):
     return l
 
 
-def rolling_hash(string):
-    hashed_str = 0
-    k = len(string)
-    for i in range(k):
-        hashed_str += string[i] * pow(A, k - i - 1, B)
-        hashed_str %= B
-    return hashed_str
-
-
-def hash_substrings(string, hashed_str):
-    k = len(string)
-    for i in range(k):
-        yield (hashed_str - string[i] * pow(A, k - i - 1, B)) % B
