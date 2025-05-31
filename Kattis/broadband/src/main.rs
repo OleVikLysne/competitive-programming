59877bde274c288e6a0e4f19d3ae46242e7440c2
OleVikLysne <oleviklysne@gmail.com>
2025-05-31 18:04:46 +0200

    solved b road band

---
+++ Kattis/broadband/src/main.rs
@@ -0,0 +1,176 @@
+use std::fmt::Display;
+use std::io::Read;
+use std::str::{Chars, FromStr, SplitAsciiWhitespace};
+
+const MAX: usize = 2000;
+
+fn main() {
+    let mut io = IO::new();
+    let (m, n, k, s): (usize, usize, usize, f32) = io.r4();
+    let mut arr= [f32::MAX; MAX];
+    let mut i = 0;
+    for _ in 0..2 {
+        for x in io.line() {
+            arr[i] = x;
+            i += 1
+        }
+    }
+    let n = m+n;
+    arr[..n].sort_by(|a, b| a.partial_cmp(b).unwrap());
+    let mut ap = [[f32::MAX; MAX]; MAX];
+    let mut dp = [[f32::MAX; MAX]; 101];
+    for i in 0..n {
+        let mut tot = 0.0;
+        for j in i..n {
+            tot += arr[j];
+            let a = tot / (j-i+1) as f32;
+            let mut d = 0.0;
+            for l in i..j+1 {
+                d += (arr[l]-a).powi(2);
+            }
+            ap[i][j] = d;
+        }
+    }
+    for i in 0..n {
+        dp[1][i] = ap[0][i];
+    }
+    for l in 2..k+1 {
+        for i in l-1..n {
+            for j in i..n {
+                dp[l][j] = dp[l][j].min(dp[l-1][i-1] + ap[i][j]); 
+            }
+        }
+    }
+    println!("{}", dp[k][n-1] + n as f32 * (s/2.0).powi(2));
+}
+
+struct IO {
+    buf: String,
+    stdin: std::io::Stdin,
+}
+
+#[allow(dead_code)]
+impl IO {
+    fn new() -> Self {
+        IO {
+            buf: String::new(),
+            stdin: std::io::stdin(),
+        }
+    }
+
+    fn _rl(&mut self) {
+        self.buf.clear();
+        let _ = self.stdin.read_line(&mut self.buf);
+    }
+
+    fn parse<T: FromStr>(&self, s: &str) -> T {
+        unsafe { s.parse().unwrap_unchecked() }
+    }
+
+    fn parse_next<T: FromStr>(&self, line_split: &mut SplitAsciiWhitespace) -> T {
+        unsafe { self.parse(line_split.next().unwrap_unchecked()) }
+    }
+
+    fn line<T: FromStr>(&mut self) -> impl Iterator<Item = T> + '_ {
+        self._rl();
+        return self.buf.split_ascii_whitespace().map(|x| self.parse(x));
+    }
+
+    fn linenl<T: FromStr>(&mut self, n: usize) -> impl Iterator<Item = T> + '_ {
+        return (0..n).map(|_| self.r());
+    }
+    
+    fn vec<T: FromStr>(&mut self) -> Vec<T> {
+        return self.line().collect();
+    }
+
+    fn vecnl<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        return self.linenl(n).collect();
+    }
+
+    fn chars(&mut self) -> Chars {
+        self._rl();
+        return self.buf.trim().chars();
+    }
+
+    fn all(&mut self) -> String {
+        self.buf.clear();
+        let _ = self.stdin.read_to_string(&mut self.buf);
+        return self.buf.trim().to_string();
+    }
+
+    fn print_vec<T: Display>(&self, vec: &[T]) {
+        for x in vec {
+            print!("{} ", *x);
+        }
+    }
+
+    fn r<T: FromStr>(&mut self) -> T {
+        self._rl();
+        self.parse(self.buf.trim())
+    }
+
+    fn r2<T1, T2>(&mut self) -> (T1, T2)
+    where
+        T1: FromStr,
+        T2: FromStr,
+    {
+        self._rl();
+        let mut line_split = self.buf.split_ascii_whitespace();
+        (
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+        )
+    }
+
+    fn r3<T1, T2, T3>(&mut self) -> (T1, T2, T3)
+    where
+        T1: FromStr,
+        T2: FromStr,
+        T3: FromStr,
+    {
+        self._rl();
+        let mut line_split = self.buf.split_ascii_whitespace();
+        (
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+        )
+    }
+
+    fn r4<T1, T2, T3, T4>(&mut self) -> (T1, T2, T3, T4)
+    where
+        T1: FromStr,
+        T2: FromStr,
+        T3: FromStr,
+        T4: FromStr,
+    {
+        self._rl();
+        let mut line_split = self.buf.split_ascii_whitespace();
+        (
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+        )
+    }
+
+    fn r5<T1, T2, T3, T4, T5>(&mut self) -> (T1, T2, T3, T4, T5)
+    where
+        T1: FromStr,
+        T2: FromStr,
+        T3: FromStr,
+        T4: FromStr,
+        T5: FromStr,
+    {
+        self._rl();
+        let mut line_split = self.buf.split_ascii_whitespace();
+        (
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+            self.parse_next(&mut line_split),
+        )
+    }
+}
