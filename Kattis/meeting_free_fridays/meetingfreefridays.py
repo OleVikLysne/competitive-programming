d13d37fab09dd4e74eceb955cf9fb1be9ab130ab
OleVikLysne <oleviklysne@gmail.com>
2025-11-16 21:28:50 +0100

    solved meeting free fridays

---
+++ Kattis/meeting_free_fridays/meetingfreefridays.py
@@ -0,0 +1,27 @@
+import sys; input=sys.stdin.readline
+import bisect
+
+N, T, K = map(int, input().split())
+intervals = [tuple(map(int, input().split())) for _ in range(N)]
+intervals.sort(key=lambda x: x[0])
+intervals.append((T, T))
+
+m = [bisect.bisect_left(intervals, intervals[j][1], key = lambda x: x[0]) for j in range(N)]
+dp = [[-2**60]*(N+1) for _ in range(N+1)]
+dp[0][0] = intervals[0][0]
+res = 0
+for i in range(N):
+    for j in range(N):
+        k = m[j]
+        dp[i][j+1] = max(
+            dp[i][j+1],
+            dp[i][j] + intervals[j+1][0] - intervals[j][0]
+        )
+        dp[i+1][k] = max(
+            dp[i+1][k],
+            dp[i][j] + intervals[k][0] - intervals[j][1]
+        )
+        if i + 1 > res and dp[i+1][j+1] + T - intervals[j+1][0] >= K:
+            res = i + 1
+
+print(res)
